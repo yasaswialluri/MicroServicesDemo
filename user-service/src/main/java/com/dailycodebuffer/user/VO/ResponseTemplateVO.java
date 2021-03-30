15fc9c1f77a99a76690f5731600ed2e0d1d3b4bf
ayasaswi <ayasaswi@LIN66009589.corp.capgemini.com>
2021-03-30 17:03:31 +0530

    user service

---
+++ user-service/src/main/java/com/dailycodebuffer/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.dailycodebuffer.user.VO;
+
+import com.dailycodebuffer.user.entity.Users;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private Users user;
+    private Department department;
+}
